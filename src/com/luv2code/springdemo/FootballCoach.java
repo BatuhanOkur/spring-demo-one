e5d5d7c4118c8729519a2151c9b6ea8fcf29ed4c
Batuhan Okur <batuhanokur2209@gmail.com>
2022-09-04 00:07:13 +0300

    Constructor and getDailyFortune methods have been created

--- src/com/luv2code/springdemo/FootballCoach.java
+++ src/com/luv2code/springdemo/FootballCoach.java
@@ -1,10 +1,21 @@
 package com.luv2code.springdemo;
 
 public class FootballCoach implements Coach {
+	private FortuneService fortuneService;
+	
+	public FootballCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Spend one hour on penalty practice";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return "Football Coach: " + fortuneService.getFortune();
 	}
 
 }
